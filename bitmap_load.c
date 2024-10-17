2dfc249ff36547517bd37584c25c5e62be627210
xp5-org <kylematthewholst@gmail.com>
2024-10-17 11:03:55 -0700

    duplicate
    removed duplicate registers definitions

--- bitmap_load.c
+++ bitmap_load.c
@@ -2,21 +2,13 @@
 #include <stdint.h>
 #include <cbm.h>
 
-// VIC registers
-#define D011 (*(char*)0xD011)
-#define D016 (*(char*)0xD016)
-#define DD00 (*(char*)0xDD00)
-#define D018 (*(char*)0xD018)
-#define D020 (*(char*)0xD020)
-#define D021 (*(char*)0xD021)
-
 char *imgsrc;
 char *imgdest;
 
 extern const char bitmapdata[];
 
 void copymem(char* src, char* dest, int length) {
-    int i;  // Declare at the start
+    int i;
     for (i = 0; i < length; i++) {
         *(dest + i) = *(src + i);
     }
@@ -26,6 +18,7 @@ int main() {
     char *imgsrc;
     char *imgdest;
 
+    // VIC registers
     (*(char*)0xD011) = 0x3B;  // 25 rows=
     (*(char*)0xD016) = 0x1B;  // 40 columns multicolor mode
     (*(char*)0xDD00) = 0x02;  // VIC bank ($4000-$7FFF)
